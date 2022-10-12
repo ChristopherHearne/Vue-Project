ac4bea10c3e52d59b16d0bbd04b802c6704a7e63
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-10-12 14:04:17 +0200

    pushing robot builder to github

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import HomePage from './home/HomePage.vue'
+import RobotBuilder from './build/RobotBuilder.vue'
+import Search from './search/Search.vue';
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png">
+          Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <RobotBuilder /> 
+  </main>
+</template>
+
+
+<style>
+  body{
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed; 
+  }
+  main{
+    margin: 0 auto; 
+    padding: 30px; 
+    background-color: #fff;
+    width: 1024px; 
+    min-height: 300px; 
+  }
+</style>
+
+
+<style scoped>
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" />
+    </div>
+    <div class="get-started">
+      <a href="">{{msg}}</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home{
+  text-align: center;
+}
+
+.robot{
+  height: 300px; 
+}
+</style>
